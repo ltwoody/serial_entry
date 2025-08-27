3cfd68b2500076e140033c37de671ab238945378
Woody <alfon-20@hotmail.com>
2025-08-27 17:13:30 +0700

    update cookkies time

--- app/api/login/route.ts
+++ app/api/login/route.ts
@@ -32,7 +32,7 @@ export async function POST(req: NextRequest) {
       path: '/',
       secure: process.env.NODE_ENV === 'production',
       sameSite: 'lax',
-      maxAge: 60 * 60 * 24,
+      maxAge: 60 * 60 * 24*30,
     });
 
     response.cookies.set({
@@ -42,7 +42,7 @@ export async function POST(req: NextRequest) {
       path: '/',
       secure: process.env.NODE_ENV === 'production',
       sameSite: 'lax',
-      maxAge: 60 * 60 * 24,
+      maxAge: 60 * 60 * 24*30,
     });
 
     response.cookies.set({
@@ -52,7 +52,7 @@ export async function POST(req: NextRequest) {
       path: '/',
       secure: process.env.NODE_ENV === 'production',
       sameSite: 'lax',
-      maxAge: 60 * 60 * 24,
+      maxAge: 60 * 60 * 24*30,
     });
 
     return response;
